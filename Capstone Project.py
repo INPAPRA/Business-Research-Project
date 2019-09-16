87549ef89a8c6cf14b6c7dd7fa4fa869b8218a28
PARITOSH PRAMANIK <54093721+INPAPRA@users.noreply.github.com>
2019-09-17 00:19:29 +0530

    Add files via upload

---
+++ Capstone Project.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# I am taking IBM Dta Science Course on Coursera. This notebook is to do Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print(" Hallo Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
